17efafd346a46fd2e8ff69e7653d7b6bb38dda1e
SiriChandhana06 <sirisiri3006@gmail.com>
2024-10-09 21:11:42 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import Navbar from './Navbar'
 import SecureTransaction from './SecureTransaction';
-import Dashboard from './Dashboard';
+// import Dashboard from './Dashboard';
 
 const Home = () => {
     return (
@@ -35,9 +35,9 @@ const Home = () => {
             <div>
                 <SecureTransaction/>
             </div>
-            <div>
+            {/* <div>
                 <Dashboard />
-            </div>
+            </div> */}
             <div className='text-black my-4'>
                 <h1 className='text-center'>support@securetrace.io - securedapp.io - 2024 </h1>
                 <h1 className='text-center'>Terms od service and privacy</h1>
