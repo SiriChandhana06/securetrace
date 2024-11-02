56066c9a87f66e9246778295e55f42e079b8ff4f
Naveen-gn <naveen2003gn@gmail.com>
2024-11-02 21:12:32 +0530

    Portfolio page dynamic api call change

--- src/Components/AddressCard.jsx
+++ src/Components/AddressCard.jsx
@@ -3,12 +3,10 @@ import { FaRegCopy, } from 'react-icons/fa';
 import img from '../Assests/person.png';
 import { FaShareNodes } from 'react-icons/fa6';
 import { useState } from 'react';
-// import Portfolio from './Portfolio';
-import Transfer from './Transfer';
-import { useEffect } from 'react';
 import Port from "../Assests/Portfolio.png";
 import { TiArrowSortedDown } from "react-icons/ti";
 import axios from "axios";
+import btc from '../Assests/Bitcoin.png';
 
 const AddressCard = () => {
 
@@ -29,57 +27,31 @@ const AddressCard = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalValue, setTotalValue] = useState(0);
 
+  const transferData = [
+    {
+        icon: btc,
+        time: '0 day ago',
+        from: '0xD78...35Cx',
+        to: '0xD78...35Cx',
+        value: '0.00K',
+        token: 'USDT',
+        usd: '$0.00K',
+    },
 
+];
 
-  useEffect(() => {
-    const fetchPortfolioTracker = async () => {
-      setLoading(true);
+const [currentPage1, setCurrentPage1] = useState(1);
+const rowsPerPage1 = 10;
+const [transfers, setTransfers] = useState([]);
 
-      try {
-        const response = await axios.get(
-          "https://caiman-wanted-fox.ngrok-free.app/fetch-address-details/0x04b21735E93Fa3f8df70e2Da89e6922616891a88",
-          {
-            headers: {
-              "ngrok-skip-browser-warning": "true",
-
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        SetPortfolioData(response.data.tokens[0]);
-        console.log(response.data.tokens[0]);
-
-        const tokens = response.data.tokens[0];
-        // setPortfolioData(tokens);
-
-        // Calculate total value
-        const total = tokens.reduce((sum, item) => {
-          const price = parseFloat(item.tokenPrice);
-          const holdings = parseFloat(item.tokenBalance);
-          return sum + (price * holdings);
-        }, 0);
-
-        setTotalValue(total.toFixed(2));
-
-
-        setLoading(false);
-      } catch (error) {
-        console.log("error", error);
-
-        setLoading(false);
-      }
+const totalPages1 = Math.ceil(transfers.length / rowsPerPage1);
+    const currentRows1 = transfers.slice((currentPage1 - 1) * rowsPerPage1, currentPage1 * rowsPerPage1);
+    const handlePageChange1 = (pageNumber) => {
+        if (pageNumber >= 1 && pageNumber <= totalPages1) {
+            setCurrentPage1(pageNumber);
+        }
     };
-
-    fetchPortfolioTracker();
-  }, []);
-
-  useEffect(
-    () => {
-      console.log("portfolio from:", portfolioData.from);
-    }, [portfolioData]
-  )
-
+  
 
   const jsonData = {
     "0x04b21735E93Fa3f8df70e2Da89e6922616891a88": {
@@ -102,29 +74,91 @@ const AddressCard = () => {
     // alert('Address copied to clipboard!');
   };
 
-  const handleScanNow = () => {
+  const handleScanNow = async() => {
+    setLoading(true);
     if (!inputValue) {
       alert('Please enter a contract address.');
+      setLoading(false);
       return;
     }
 
     if (!isValidEthereumAddressOrTxHash(inputValue)) {
       alert('Invalid Ethereum address. Please enter a valid input.');
+      setLoading(false);
       return;
     }
 
-    const foundData = jsonData[inputValue];
-    if (foundData) {
-      setCardData({
-        address: inputValue,
-        amount: foundData.amount,
-        greenAmount: foundData.greenAmount,
-      });
+    try {
+      const response = await axios.get(
+        `https://caiman-wanted-fox.ngrok-free.app/fetch-address-details/${inputValue}`,
+        {
+          headers: {
+            "ngrok-skip-browser-warning": "true",
+
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      SetPortfolioData(response.data.tokens[0]);
+      console.log("Portfolio data:", response.data.tokens[0]);
+
+      const tokens = response.data.tokens[0];
+      // setPortfolioData(tokens);
+
+      // Calculate total value
+      const total = tokens.reduce((sum, item) => {
+        const price = parseFloat(item.tokenPrice);
+        const holdings = parseFloat(item.tokenBalance);
+        return sum + (price * holdings);
+      }, 0);
+
+      setTotalValue(total.toFixed(2));
       setIsPortfolioVisible(true);
-    } else {
-      alert('Data not found for the given address value.');
+
+      setLoading(false);
+    } catch (error) {
+      console.log("error", error);
+
+      setLoading(false);
     }
+
+    try {
+      const response1 = await axios.get(
+          `https://caiman-wanted-fox.ngrok-free.app/token-transfers/${inputValue}`,
+          {
+              headers: {
+                  "ngrok-skip-browser-warning": "true",
+
+                  "Content-Type": "application/json",
+              },
+          }
+      );
+
+      setTransfers(response1.data.from[0]);
+      console.log("Transfres from:",response1.data.from[0]);
+
+      setLoading(false);
+  } catch (error) {
+      console.log("error", error);
+
+      setLoading(false);
+  }
+
+    // const foundData = jsonData[inputValue];
+    // if (foundData) {
+    //   setCardData({
+    //     address: inputValue,
+    //     amount: foundData.amount,
+    //     greenAmount: foundData.greenAmount,
+    //   });
+    //   setIsPortfolioVisible(true);
+    // } else {
+    //   alert('Data not found for the given address value.');
+    // }
     setInputValue('');
+
+
   };
 
   const data = [
@@ -163,16 +197,24 @@ const AddressCard = () => {
           <input
             type="text"
             value={inputValue}
+            disabled={loading}
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="Enter address value"
             className="py-3 px-4 rounded-xl border border-gray-300 shadow-lg focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent mb-4 sm:mb-0 mx-2 w-full"
           />
-          <button onClick={handleScanNow} className="bg-green-500 w-56 lg:w-40 text-black font-semibold py-3 px-8 rounded-xl shadow-md hover:bg-green-600 transition-all duration-300">
+          <button onClick={handleScanNow} disabled={loading} className="bg-green-500 w-56 lg:w-40 text-black font-semibold py-3 px-8 rounded-xl shadow-md hover:bg-green-600 transition-all duration-300">
             Scan Now
           </button>
         </div>
       </div>
-      {cardData && (
+{loading&&(
+  <div className="flex items-center justify-center mt-6">
+  <div className="animate-spin rounded-full h-14 w-14 border-t-2 border-b-2 border-green-700"></div>
+</div>
+)}
+
+
+      {cardData && !loading && (
         <div className="mx-4 md:mx-32 my-10 border border-black rounded-lg shadow-lg shadow-gray-500 p-4 bg-white flex flex-col lg:flex-row items-center gap-4">
           <div className="flex-1 ml-0 md:ml-4">
             <div className="flex items-center">
@@ -196,7 +238,8 @@ const AddressCard = () => {
         </div>
       )}
 
-      {isPortfolioVisible && (
+
+      {isPortfolioVisible && !loading && (
         <div className='mx-4 md:mx-32'>
           {/* <div className="">
             <Portfolio />
@@ -281,9 +324,9 @@ const AddressCard = () => {
                     {currentRows && currentRows.length > 0 ? (
                       currentRows.map((item, index) => {
                         const asset = item.tokenName;
-                        const price = item.tokenPrice;
-                        const holdings = item.tokenBalance;
-                        const value = (price * holdings); 
+                        const price = parseFloat(item.tokenPrice).toFixed(2);
+                        const holdings = parseFloat(item.tokenBalance).toFixed(4);
+                        const value = (price * holdings).toFixed(2); // Calculate value as price * balance
 
                         return (
                           <tr key={index} className="border-t h-12 odd:bg-[#F4F4F4] even:bg-white">
@@ -311,7 +354,117 @@ const AddressCard = () => {
             </div>
           </div>
           <div className="mt-10 ">
-            <Transfer />
+          <div className="overflow-x-hidden bg-white p-6 rounded-xl border border-black shadow-md shadow-gray-500" id="hide-scrollbar">
+            <div className="">
+                <div className='flex'>
+                    <h3 className="text-2xl font-semibold mt-1 mb-4">Transfers</h3>
+                    <div className="flex items-center mb-4">
+                        <button
+                            className={`px-4 py-2 font-bold ${currentPage1 === 1 ? 'cursor-not-allowed opacity-50 ' : 'cursor-pointer'}`}
+                            onClick={() => handlePageChange1(currentPage1 - 1)}
+                            disabled={currentPage1 === 1}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024"><path fill="black" d="M685.248 104.704a64 64 0 0 1 0 90.496L368.448 512l316.8 316.8a64 64 0 0 1-90.496 90.496L232.704 557.248a64 64 0 0 1 0-90.496l362.048-362.048a64 64 0 0 1 90.496 0" /></svg>
+                        </button>
+                        <span className='font-bold text-xl'>
+                            {currentPage1} / {totalPages1}
+                        </span>
+                        <button
+                            className={`px-4 py-2 font-bold ${currentPage1 === totalPages1 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
+                            onClick={() => handlePageChange1(currentPage1 + 1)}
+                            disabled={currentPage1 === totalPages1}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 12 24"><path fill="black" fill-rule="evenodd" d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414" /></svg>
+                        </button>
+                    </div>
+                </div>
+                <div className="overflow-x-scroll"  id="hide-scrollbar">
+                    <table className="w-full text-center">
+                        <thead className=''>
+                            <tr className="text-gray-500 h-10">
+                                <th className='flex justify-center items-center space-x-2 px-4'><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" d="M14.78 3.653a3.936 3.936 0 1 1 5.567 5.567l-3.627 3.627a3.936 3.936 0 0 1-5.88-.353a.75.75 0 0 0-1.18.928a5.436 5.436 0 0 0 8.12.486l3.628-3.628a5.436 5.436 0 1 0-7.688-7.688l-3 3a.75.75 0 0 0 1.06 1.061z" /><path fill="black" d="M7.28 11.153a3.936 3.936 0 0 1 5.88.353a.75.75 0 0 0 1.18-.928a5.436 5.436 0 0 0-8.12-.486L2.592 13.72a5.436 5.436 0 1 0 7.688 7.688l3-3a.75.75 0 1 0-1.06-1.06l-3 3a3.936 3.936 0 0 1-5.567-5.568z" /></svg></th>
+                                <th className=' px-4'>
+                                    <div className="flex justify-center items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" fill-rule="evenodd" d="m12.6 11.503l3.891 3.891l-.848.849L11.4 12V6h1.2zM12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-1.2a8.8 8.8 0 1 0 0-17.6a8.8 8.8 0 0 0 0 17.6" /></svg>
+                                        <h1>Time</h1>
+                                    </div>
+                                </th>
+                                <th className=' px-6'>
+                                    <div className="flex justify-center items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                                        <h1>From</h1>
+                                    </div>
+                                </th>
+                                <th className='px-6'>
+                                    <div className="flex justify-center items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                                        <h1>To</h1>
+                                    </div>
+                                </th>
+                                <th className=' px-4'>
+                                    <div className="flex justify-center items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                                        <h1>Value</h1>
+                                    </div>
+                                </th>
+                                <th className=' px-4'>
+                                    <div className="flex justify-center items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                                        <h1>Token</h1>
+                                    </div>
+                                </th>
+                                <th className=' px-4'>
+                                    <div className="flex justify-center items-center space-x-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                                        <h1>Amount</h1>
+                                    </div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className=" text-center">
+                            {currentRows1 && currentRows1.length > 0 ?
+                                (currentRows1.map((transfer, index) => {
+                                    const { icon, timestamp, from, to, value, tokenName, tokenPrice } = transfer;
+                                    return (
+                                        <tr key={index} className="border-t  h-12  text-center bg-red-600 odd:bg-[#F4F4F4] even:bg-white px-2 py-2">
+                                            <td className='flex justify-center items-center mt-2 px-4'><img src={icon} alt={tokenName} /></td>
+                                            {/* <td className="text-green-500 me-3 px-4">{timestamp}</td> */}
+                                            <td className="text-green-500 me-3 px-4">{new Date(timestamp).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })}</td>
+                                            {/* <td className="me-3 px-4">{from}</td>
+                                            <td className="me-3 px-4">{to}</td> */}
+                                            <td className="me-3 px-4">{from.slice(0, 5) + "..." + from.slice(-4)}</td>
+                                            <td className="me-3 px-4">{to.slice(0, 5) + "..." + to.slice(-4)}</td>
+                                            {/* <td className='text-green-500'>{transfer.value}</td> */}
+                                            <td className="text-green-500 px-4">
+                                                {tokenPrice}
+                                            </td>
+                                            <td className="px-4">{tokenName}</td>
+                                            <td className="px-4">{value}</td>
+                                        </tr>
+                                    );
+                                }))
+                                :
+                                (
+                                    <tr className="border-t h-12 odd:bg-[#F4F4F4] even:bg-white ">
+                                        <td className='flex justify-center items-center mt-2'><img src={btc} alt="Token Name" /></td>
+                                        <td className="text-green-500 text-center">0 days ago</td>
+                                        <td className="text-center">0000....000</td>
+                                        <td className="text-center">0000....000</td>
+                                        <td className='text-green-500'>0.00</td>
+                                        {/* <td className="text-green-500">
+                                            0.00
+                                        </td> */}
+                                        <td>BTC</td>
+                                        <td>$0.00</td>
+                                    </tr>
+                                )
+                            }
+
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
           </div>
         </div>
       )}
