6c0960ae776eccc144dbb75b7ca51c0bedc9d709
SiriChandhana06 <sirisiri3006@gmail.com>
2024-11-16 20:39:27 +0530

    Visualizer Update

--- src/Components/Visualizer.jsx
+++ src/Components/Visualizer.jsx
@@ -66,7 +66,7 @@ const Visualizer = () => {
         setValidationMessage('Error retrieving data.');
       }
       setLoading(false);
-    } else if(validateTransactionHash(value)) {
+    } else if (validateTransactionHash(value)) {
       setLoading(true);
 
       try {
@@ -518,12 +518,19 @@ const Visualizer = () => {
     <div>
       <div className="flex flex-col items-center justify-center py-10 px-4 bg-white">
         {!isInputEntered && (
-          <>
-            <h1 className="text-3xl font-bold text-center mb-4">SecureTrace</h1>
-            <p className="text-center text-gray-600 mb-6 max-w-2xl font-semibold">
-              SecureTrace analyzes transaction data using blockchain forensic techniques, enhancing the detection of intricate patterns and potential vulnerabilities.
-            </p>
-          </>
+            <div className='flex items-center justify-center'>
+              <div className=' mt-10 md:mt-20'>
+                <h1 className="text-3xl font-bold text-center mb-4">
+                  SecureTrace Visualizer
+                </h1>
+
+                <p className="text-center text-gray-600 mb-6 max-w-2xl font-semibold">
+                  SecureTrace analyzes transaction data using specialized blockchain
+                  forensic techniques, enhancing the detection of intricate patterns
+                  and potential vulnerabilities.
+                </p>
+              </div>
+            </div>
         )}
         <div className="flex flex-col sm:flex-row items-center w-full md:max-w-3xl ">
           <input
@@ -547,122 +554,126 @@ const Visualizer = () => {
         </div>
       </div>
       <div>
-      {isInputEntered && (
-        // <div className="mt-10 mx-20">
+        {isInputEntered && (
+          // <div className="mt-10 mx-20">
           <div className='mx-4 md:mx-32 my-10'>
-          <div className="overflow-x-hidden bg-white p-6 rounded-xl border border-black shadow-md shadow-gray-500" id="hide-scrollbar">
-            <div className="">
-              <div className='flex'>
-                <h3 className="text-2xl font-semibold mt-1 mb-4">Transfers</h3>
-                <div className="flex items-center mb-4">
-                  <button
-                    className={`px-4 py-2 font-bold ${currentPage1 === 1 ? 'cursor-not-allowed opacity-50 ' : 'cursor-pointer'}`}
-                    onClick={() => handlePageChange1(currentPage1 - 1)}
-                    disabled={currentPage1 === 1}
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024"><path fill="black" d="M685.248 104.704a64 64 0 0 1 0 90.496L368.448 512l316.8 316.8a64 64 0 0 1-90.496 90.496L232.704 557.248a64 64 0 0 1 0-90.496l362.048-362.048a64 64 0 0 1 90.496 0" /></svg>
-                  </button>
-                  <span className='font-bold text-xl'>
-                    {currentPage1} / {totalPages1}
-                  </span>
-                  <button
-                    className={`px-4 py-2 font-bold ${currentPage1 === totalPages1 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
-                    onClick={() => handlePageChange1(currentPage1 + 1)}
-                    disabled={currentPage1 === totalPages1}
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 12 24"><path fill="black" fill-rule="evenodd" d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414" /></svg>
-                  </button>
+            <div className="overflow-x-hidden bg-white p-6 rounded-xl border border-black shadow-md shadow-gray-500" id="hide-scrollbar">
+              <div className="">
+                <div className='flex'>
+                  <h3 className="text-2xl font-semibold mt-1 mb-4">Transfers</h3>
+                  <div className="flex items-center mb-4">
+                    <button
+                      className={`px-4 py-2 font-bold ${currentPage1 === 1 ? 'cursor-not-allowed opacity-50 ' : 'cursor-pointer'}`}
+                      onClick={() => handlePageChange1(currentPage1 - 1)}
+                      disabled={currentPage1 === 1}
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1024 1024"><path fill="black" d="M685.248 104.704a64 64 0 0 1 0 90.496L368.448 512l316.8 316.8a64 64 0 0 1-90.496 90.496L232.704 557.248a64 64 0 0 1 0-90.496l362.048-362.048a64 64 0 0 1 90.496 0" /></svg>
+                    </button>
+                    <span className='font-bold text-xl'>
+                      {currentPage1} / {totalPages1}
+                    </span>
+                    <button
+                      className={`px-4 py-2 font-bold ${currentPage1 === totalPages1 ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'}`}
+                      onClick={() => handlePageChange1(currentPage1 + 1)}
+                      disabled={currentPage1 === totalPages1}
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 12 24"><path fill="black" fill-rule="evenodd" d="M10.157 12.711L4.5 18.368l-1.414-1.414l4.95-4.95l-4.95-4.95L4.5 5.64l5.657 5.657a1 1 0 0 1 0 1.414" /></svg>
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <div className="overflow-x-scroll" id="hide-scrollbar">
-                <table className="w-full text-center">
-                  <thead className=''>
-                    <tr className="text-gray-500 h-10">
-                      <th className='flex justify-center items-center space-x-2 px-4'><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" d="M14.78 3.653a3.936 3.936 0 1 1 5.567 5.567l-3.627 3.627a3.936 3.936 0 0 1-5.88-.353a.75.75 0 0 0-1.18.928a5.436 5.436 0 0 0 8.12.486l3.628-3.628a5.436 5.436 0 1 0-7.688-7.688l-3 3a.75.75 0 0 0 1.06 1.061z" /><path fill="black" d="M7.28 11.153a3.936 3.936 0 0 1 5.88.353a.75.75 0 0 0 1.18-.928a5.436 5.436 0 0 0-8.12-.486L2.592 13.72a5.436 5.436 0 1 0 7.688 7.688l3-3a.75.75 0 1 0-1.06-1.06l-3 3a3.936 3.936 0 0 1-5.567-5.568z" /></svg></th>
-                      <th className=' px-4'>
-                        <div className="flex justify-center items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" fill-rule="evenodd" d="m12.6 11.503l3.891 3.891l-.848.849L11.4 12V6h1.2zM12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-1.2a8.8 8.8 0 1 0 0-17.6a8.8 8.8 0 0 0 0 17.6" /></svg>
-                          <h1>Time</h1>
-                        </div>
-                      </th>
-                      <th className=' px-6'>
-                        <div className="flex justify-center items-center space-x-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
-                          <h1>From</h1>
-                        </div>
-                      </th>
-                      <th className='px-6'>
-                        <div className="flex justify-center items-center space-x-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
-                          <h1>To</h1>
-                        </div>
-                      </th>
-                      <th className=' px-4'>
-                        <div className="flex justify-center items-center space-x-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
-                          <h1>Value</h1>
-                        </div>
-                      </th>
-                      <th className=' px-4'>
-                        <div className="flex justify-center items-center space-x-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
-                          <h1>Token</h1>
-                        </div>
-                      </th>
-                      <th className=' px-4'>
-                        <div className="flex justify-center items-center space-x-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
-                          <h1>Amount</h1>
-                        </div>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className=" text-center">
-                    {currentRows1 && currentRows1.length > 0 ?
-                      (currentRows1.map((transfer, index) => {
-                        const { logo, timestamp, from, to, value, tokenName, tokenPrice } = transfer;
-                        return (
-                          <tr key={index} className="border-t  h-12  text-center bg-red-600 odd:bg-[#F4F4F4] even:bg-white px-2 py-2">
-                            <td className='flex justify-center items-center mt-2 px-4'><img src={logo} alt={tokenName} /></td>
-                            {/* <td className="text-green-500 me-3 px-4">{timestamp}</td> */}
-                            <td className="text-green-500 me-3 px-4">{new Date(timestamp).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })}</td>
-                            {/* <td className="me-3 px-4">{from}</td>
+                <div className="overflow-x-scroll" id="hide-scrollbar">
+                  <table className="w-full text-center">
+                    <thead className=''>
+                      <tr className="text-gray-500 h-10">
+                        <th className='flex justify-center items-center space-x-2 px-4'><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" d="M14.78 3.653a3.936 3.936 0 1 1 5.567 5.567l-3.627 3.627a3.936 3.936 0 0 1-5.88-.353a.75.75 0 0 0-1.18.928a5.436 5.436 0 0 0 8.12.486l3.628-3.628a5.436 5.436 0 1 0-7.688-7.688l-3 3a.75.75 0 0 0 1.06 1.061z" /><path fill="black" d="M7.28 11.153a3.936 3.936 0 0 1 5.88.353a.75.75 0 0 0 1.18-.928a5.436 5.436 0 0 0-8.12-.486L2.592 13.72a5.436 5.436 0 1 0 7.688 7.688l3-3a.75.75 0 1 0-1.06-1.06l-3 3a3.936 3.936 0 0 1-5.567-5.568z" /></svg></th>
+                        <th className=' px-4'>
+                          <div className="flex justify-center items-center space-x-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" fill-rule="evenodd" d="m12.6 11.503l3.891 3.891l-.848.849L11.4 12V6h1.2zM12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-1.2a8.8 8.8 0 1 0 0-17.6a8.8 8.8 0 0 0 0 17.6" /></svg>
+                            <h1>Time</h1>
+                          </div>
+                        </th>
+                        <th className=' px-6'>
+                          <div className="flex justify-center items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                            <h1>From</h1>
+                          </div>
+                        </th>
+                        <th className='px-6'>
+                          <div className="flex justify-center items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                            <h1>To</h1>
+                          </div>
+                        </th>
+                        <th className=' px-4'>
+                          <div className="flex justify-center items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                            <h1>Value</h1>
+                          </div>
+                        </th>
+                        <th className=' px-4'>
+                          <div className="flex justify-center items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                            <h1>Token</h1>
+                          </div>
+                        </th>
+                        <th className=' px-4'>
+                          <div className="flex justify-center items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96" /></svg>
+                            <h1>Amount</h1>
+                          </div>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className=" text-center">
+                      {currentRows1 && currentRows1.length > 0 ?
+                        (currentRows1.map((transfer, index) => {
+                          const { logo, timestamp, from, to, value, tokenName, tokenPrice } = transfer;
+                          return (
+                            <tr key={index} className="border-t  h-12  text-center bg-red-600 odd:bg-[#F4F4F4] even:bg-white px-2 py-2">
+                              <td className='flex justify-center items-center mt-2 px-4'><img src={logo} alt={tokenName} /></td>
+                              {/* <td className="text-green-500 me-3 px-4">{timestamp}</td> */}
+                              <td className="text-green-500 me-3 px-4">{new Date(timestamp).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })}</td>
+                              {/* <td className="me-3 px-4">{from}</td>
                                             <td className="me-3 px-4">{to}</td> */}
-                            <td className="me-3 px-4">{from.slice(0, 5) + "..." + from.slice(-4)}</td>
-                            <td className="me-3 px-4">{to.slice(0, 5) + "..." + to.slice(-4)}</td>
-                            {/* <td className='text-green-500'>{transfer.value}</td> */}
-                            <td className="text-green-500 px-4">
-                              {parseFloat(tokenPrice).toFixed(2)}
-                            </td>
-                            <td className="px-4">{tokenName}</td>
-                            <td className="px-4">{parseFloat(value).toFixed(2)}</td>
-                          </tr>
-                        );
-                      }))
-                      :
-                      (
-                        <tr className="border-t h-12 odd:bg-[#F4F4F4] even:bg-white ">
-                          <td className='flex justify-center items-center mt-2'><img src={btc} alt="Token Name" /></td>
-                          <td className="text-green-500 text-center">0 days ago</td>
-                          <td className="text-center">0000....000</td>
-                          <td className="text-center">0000....000</td>
-                          <td className='text-green-500'>0.00</td>
-                          {/* <td className="text-green-500">
+                              <td className="me-3 px-4">{from.slice(0, 5) + "..." + from.slice(-4)}</td>
+                              <td className="me-3 px-4">{to.slice(0, 5) + "..." + to.slice(-4)}</td>
+                              {/* <td className='text-green-500'>{transfer.value}</td> */}
+                              <td className="text-green-500 px-4">
+                                {parseFloat(tokenPrice).toFixed(2)}
+                              </td>
+                              <td className="px-4">{tokenName}</td>
+                              <td className="px-4">{parseFloat(value).toFixed(2)}</td>
+                            </tr>
+                          );
+                        }))
+                        :
+                        (
+                          <tr className="border-t h-12 odd:bg-[#F4F4F4] even:bg-white ">
+                            <td className='flex justify-center items-center mt-2'><img src={btc} alt="Token Name" /></td>
+                            <td className="text-green-500 text-center">0 days ago</td>
+                            <td className="text-center">0000....000</td>
+                            <td className="text-center">0000....000</td>
+                            <td className='text-green-500'>0.00</td>
+                            {/* <td className="text-green-500">
                                             0.00
                                         </td> */}
-                          <td>BTC</td>
-                          <td>$0.00</td>
-                        </tr>
-                      )
-                    }
+                            <td>BTC</td>
+                            <td>$0.00</td>
+                          </tr>
+                        )
+                      }
 
 
-                  </tbody>
-                </table>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+      <div className='text-black'>
+        <h1 className='text-center'>support@securetrace.io - securedapp.io - 2024 </h1>
+        <h1 className='text-center'>Terms od service and privacy</h1>
       </div>
     </div>
   );
